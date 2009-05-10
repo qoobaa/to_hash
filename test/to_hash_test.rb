aa3b81ac89873c58dc844f6fab79dede24abc26e
Jakub Kuźma <qoobaa@gmail.com>
2009-05-10 21:43:29 +0200

    added more tests and more clear example

--- test/to_hash_test.rb
+++ test/to_hash_test.rb
@@ -45,7 +45,37 @@ class ToHashTest < Test::Unit::TestCase
       assert_equal hash, { :myTitle => "Project 1", :myDesc => "Project 1 description" }
     end
 
-    # TODO: more test (no time right now)
+    should "allow to include other objects in hash" do
+      hash = @project1.to_hash([:authors, :first_name])
+      assert_equal hash, { :authors => [{ :first_name => "John" }, { :first_name => "Michael" }, { :first_name => "Jimmy" }] }
+    end
+
+    should "allow to include other objects with multiple attributes in hash" do
+      hash = @project1.to_hash([:authors, :first_name, :last_name])
+      assert_equal hash, { :authors => [{ :first_name => "John", :last_name => "Doe" },
+                                        { :first_name => "Michael", :last_name => "Smith" },
+                                        { :first_name => "Jimmy", :last_name => "Parker" }] }
+    end
+
+    should "allow to include other objects with changed key" do
+      hash = @project1.to_hash(:people => [:authors, :first_name])
+      assert_equal hash, { :people => [{ :first_name => "John" }, { :first_name => "Michael" }, { :first_name => "Jimmy" }] }
+    end
+
+    should "allow to include other objects with inner keys changed" do
+      hash = @project1.to_hash(:people => [:authors, { :firstName => :first_name }])
+      assert_equal hash, { :people => [{ :firstName => "John" }, { :firstName => "Michael" }, { :firstName => "Jimmy" }] }
+    end
+
+    should "allow using different serialization methods described above" do
+      hash = @project1.to_hash(:title, [:authors, { :firstName => :first_name }])
+      assert_equal hash, { :title => "Project 1", :authors => [{ :firstName => "John" }, { :firstName => "Michael" }, { :firstName => "Jimmy" }]}
+    end
+
+    should "serialize singular nested object" do
+      hash = @project1.to_hash(:title, [:company, :name])
+      assert_equal hash, { :title => "Project 1", :company => { :name => "HAL" } }
+    end
   end
 end
 
